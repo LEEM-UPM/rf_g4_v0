1662f90a4520b4a7bda53075681b100f4a1b6edd
Jose7RT <jose7rt@gmail.com>
2026-02-07 22:18:20 +0100

    Se mueve el código "blinkgin_led" al directorio de pruebas de concepto para seguir una estructura análoga al resto de proyectos, respetando el orden en la estructura de directorios del proyecto.
    Se añade el fichero launch.json que permite depurar el MCU con VSCode  y el fichero tasks.json para agilizar las labores de desarrollo.

--- poc/blinking_led/main.c
+++ poc/blinking_led/main.c
@@ -1,22 +1,15 @@
 #include "main.h"
 #include "gpio.h"
-#include "stm32g4xx_hal_gpio.h"
 
 extern void SystemClock_Config(void);
 
-int main(void)
-{
+int main(void) {
   HAL_Init();
-
   SystemClock_Config();
-
   MX_GPIO_Init();
 
-  while (1)
-  {
-
+  while (1) {
     HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-    HAL_Delay(500);
-
+    HAL_Delay(1000);
   }
 }
